593bae06155d06f29ecee0651eaee067f7fd0f60
Michal Danko <info@michaldanko.com>
2024-11-26 09:56:45 +0100

    Setup rate limiting for API

---
+++ app/Providers/ApiServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\RateLimiter;
+
+class ApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // Rate limiting for the API
+        RateLimiter::for('api', fn () => Limit::perMinute(60));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/token', [AuthController::class, 'generateToken']);
 
 // Protected routes
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
     Route::apiResource('posts', PostController::class);
 
     Route::post('/auth/logout', [AuthController::class, 'logout']);
